279e4d21ee0c31e02d8d50f3ff246e2b8a0d3f05
Lanest <lanest.lin@oocl.com>
2025-09-05 16:30:02 +0800

    refactor: extract constant to Direction

--- src/main/java/com/afs/tdd/Application.java
+++ src/main/java/com/afs/tdd/Application.java
@@ -1,49 +1,48 @@
 package com.afs.tdd;
 
-import java.util.Objects;
-
 public class Application {
+
     public static Coordinates executiveCommand(Coordinates coordinates, String command) {
         if (command.equals("M")) {
-            if (coordinates.direction.equals("N")) {
+            if (coordinates.direction.equals(Direction.NORTH)) {
                 return new Coordinates(coordinates.x, ++coordinates.y, coordinates.direction);
             }
-            if (coordinates.direction.equals("S")) {
+            if (coordinates.direction.equals(Direction.SOUTH)) {
                 return new Coordinates(coordinates.x, --coordinates.y, coordinates.direction);
             }
-            if (coordinates.direction.equals("W")) {
+            if (coordinates.direction.equals(Direction.WEST)) {
                 return new Coordinates(--coordinates.x, coordinates.y, coordinates.direction);
             }
-            if (coordinates.direction.equals("E")) {
+            if (coordinates.direction.equals(Direction.EAST)) {
                 return new Coordinates(++coordinates.x, coordinates.y, coordinates.direction);
             }
         }
         if (command.equals("L")) {
-            if (coordinates.direction.equals("N")) {
-                return new Coordinates(coordinates.x, coordinates.y, "W");
+            if (coordinates.direction.equals(Direction.NORTH)) {
+                return new Coordinates(coordinates.x, coordinates.y, Direction.WEST);
             }
-            if (coordinates.direction.equals("S")) {
-                return new Coordinates(coordinates.x, coordinates.y, "E");
+            if (coordinates.direction.equals(Direction.SOUTH)) {
+                return new Coordinates(coordinates.x, coordinates.y, Direction.EAST);
             }
-            if (coordinates.direction.equals("W")) {
-                return new Coordinates(coordinates.x, coordinates.y, "S");
+            if (coordinates.direction.equals(Direction.WEST)) {
+                return new Coordinates(coordinates.x, coordinates.y, Direction.SOUTH);
             }
-            if (coordinates.direction.equals("E")) {
-                return new Coordinates(coordinates.x, coordinates.y, "N");
+            if (coordinates.direction.equals(Direction.EAST)) {
+                return new Coordinates(coordinates.x, coordinates.y, Direction.NORTH);
             }
         }
         if (command.equals("R")) {
-            if (coordinates.direction.equals("N")) {
-                return new Coordinates(coordinates.x, coordinates.y, "E");
+            if (coordinates.direction.equals(Direction.NORTH)) {
+                return new Coordinates(coordinates.x, coordinates.y, Direction.EAST);
             }
-            if (coordinates.direction.equals("S")) {
-                return new Coordinates(coordinates.x, coordinates.y, "W");
+            if (coordinates.direction.equals(Direction.SOUTH)) {
+                return new Coordinates(coordinates.x, coordinates.y, Direction.WEST);
             }
-            if (coordinates.direction.equals("W")) {
-                return new Coordinates(coordinates.x, coordinates.y, "N");
+            if (coordinates.direction.equals(Direction.WEST)) {
+                return new Coordinates(coordinates.x, coordinates.y, Direction.NORTH);
             }
-            if (coordinates.direction.equals("E")) {
-                return new Coordinates(coordinates.x, coordinates.y, "S");
+            if (coordinates.direction.equals(Direction.EAST)) {
+                return new Coordinates(coordinates.x, coordinates.y, Direction.SOUTH);
             }
         }
         return null;
