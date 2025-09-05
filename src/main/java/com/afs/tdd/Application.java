16549f982871865d22f5a317135267029521b527
Lanest <lanest.lin@oocl.com>
2025-09-05 15:29:20 +0800

    feat: should_return_coordinates_when_N

--- src/main/java/com/afs/tdd/Application.java
+++ src/main/java/com/afs/tdd/Application.java
@@ -1,4 +1,7 @@
 package com.afs.tdd;
 
 public class Application {
+    public static Coordinates executiveCommand(Coordinates coordinates,String command) {
+        return null;
+    }
 }
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,19 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    public void should_return_coordinates_when_N() {
+        String command = "M";
+
+        Coordinates coordinatesResult = Application.executiveCommand(
+                new Coordinates(0,0,"N"), command
+        );
+
+        assertNull(coordinatesResult);
 
     }
 }
