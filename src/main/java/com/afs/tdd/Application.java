95f5d661478a9bff54225cf221f4df2ec0bc78b6
Lanest <lanest.lin@oocl.com>
2025-09-05 16:53:59 +0800

    refactor: clean up executiveCommand

--- src/main/java/com/afs/tdd/Application.java
+++ src/main/java/com/afs/tdd/Application.java
@@ -1,50 +1,57 @@
 package com.afs.tdd;
 
 import static com.afs.tdd.Command.*;
+import static com.afs.tdd.Direction.*;
 
 public class Application {
 
     public static Coordinates executiveCommand(Coordinates coordinates, String command) {
         if (command.equals(MOVE)) {
-            if (coordinates.direction.equals(Direction.NORTH)) {
-                return new Coordinates(coordinates.x, ++coordinates.y, coordinates.direction);
-            }
-            if (coordinates.direction.equals(Direction.SOUTH)) {
-                return new Coordinates(coordinates.x, --coordinates.y, coordinates.direction);
-            }
-            if (coordinates.direction.equals(Direction.WEST)) {
-                return new Coordinates(--coordinates.x, coordinates.y, coordinates.direction);
-            }
-            if (coordinates.direction.equals(Direction.EAST)) {
-                return new Coordinates(++coordinates.x, coordinates.y, coordinates.direction);
+            switch (coordinates.direction) {
+                case NORTH -> {
+                    return new Coordinates(coordinates.x, ++coordinates.y, coordinates.direction);
+                }
+                case SOUTH -> {
+                    return new Coordinates(coordinates.x, --coordinates.y, coordinates.direction);
+                }
+                case WEST -> {
+                    return new Coordinates(--coordinates.x, coordinates.y, coordinates.direction);
+                }
+                case EAST -> {
+                    return new Coordinates(++coordinates.x, coordinates.y, coordinates.direction);
+                }
             }
         }
         if (command.equals(LEFT)) {
-            if (coordinates.direction.equals(Direction.NORTH)) {
-                return new Coordinates(coordinates.x, coordinates.y, Direction.WEST);
-            }
-            if (coordinates.direction.equals(Direction.SOUTH)) {
-                return new Coordinates(coordinates.x, coordinates.y, Direction.EAST);
-            }
-            if (coordinates.direction.equals(Direction.WEST)) {
-                return new Coordinates(coordinates.x, coordinates.y, Direction.SOUTH);
-            }
-            if (coordinates.direction.equals(Direction.EAST)) {
-                return new Coordinates(coordinates.x, coordinates.y, Direction.NORTH);
+            switch (coordinates.direction) {
+                case NORTH -> {
+                    return new Coordinates(coordinates.x, coordinates.y, WEST);
+                }
+                case SOUTH -> {
+                    return new Coordinates(coordinates.x, coordinates.y, EAST);
+                }
+                case WEST -> {
+                    return new Coordinates(coordinates.x, coordinates.y, SOUTH);
+                }
+                case EAST -> {
+                    return new Coordinates(coordinates.x, coordinates.y, NORTH);
+                }
             }
         }
         if (command.equals(RIGHT)) {
-            if (coordinates.direction.equals(Direction.NORTH)) {
-                return new Coordinates(coordinates.x, coordinates.y, Direction.EAST);
-            }
-            if (coordinates.direction.equals(Direction.SOUTH)) {
-                return new Coordinates(coordinates.x, coordinates.y, Direction.WEST);
-            }
-            if (coordinates.direction.equals(Direction.WEST)) {
-                return new Coordinates(coordinates.x, coordinates.y, Direction.NORTH);
-            }
-            if (coordinates.direction.equals(Direction.EAST)) {
-                return new Coordinates(coordinates.x, coordinates.y, Direction.SOUTH);
+            switch (coordinates.direction) {
+                case NORTH -> {
+                    return new Coordinates(coordinates.x, coordinates.y, EAST);
+                }
+                case SOUTH -> {
+                    return new Coordinates(coordinates.x, coordinates.y, WEST);
+                }
+                case WEST -> {
+                    return new Coordinates(coordinates.x, coordinates.y, NORTH);
+                }
+                case EAST -> {
+                    return new Coordinates(coordinates.x, coordinates.y, SOUTH);
+                }
             }
         }
         return null;
