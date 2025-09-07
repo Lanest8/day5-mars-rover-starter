bf532993357cfa052ac116324baae74b7b762744
Lanest <lanest.lin@oocl.com>
2025-09-07 13:44:27 +0800

    refactor[MarsRover]: map instead of for switch

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,11 +1,33 @@
 package com.afs.tdd;
 
 import java.util.List;
+import java.util.Map;
 
 import static com.afs.tdd.constant.Command.*;
 import static com.afs.tdd.constant.Direction.*;
 
 public class MarsRover {
+
+    private static final Map<String, int[]> MOVE_MAP = Map.of(
+            NORTH, new int[]{0, 1},
+            SOUTH, new int[]{0, -1},
+            WEST, new int[]{-1, 0},
+            EAST, new int[]{1, 0}
+    );
+    ;
+    private static final Map<String, String> LEFT_MAP = Map.of(
+            NORTH, WEST,
+            WEST, SOUTH,
+            SOUTH, EAST,
+            EAST, NORTH
+    );
+
+    private static final Map<String, String> RIGHT_MAP = Map.of(
+            NORTH, EAST,
+            EAST, SOUTH,
+            SOUTH, WEST,
+            WEST, NORTH
+    );
 
     public static Coordinates executiveCommand(Coordinates coordinates, String command) {
         return switch (command) {
@@ -27,43 +49,21 @@ public class MarsRover {
     }
 
     private static Coordinates executiveRightCommand(Coordinates coordinates) {
-        return switch (coordinates.getDirection()) {
-            case NORTH -> new Coordinates(coordinates.getX(), coordinates.getY(), EAST);
-            case SOUTH -> new Coordinates(coordinates.getX(), coordinates.getY(), WEST);
-            case WEST -> new Coordinates(coordinates.getX(), coordinates.getY(), NORTH);
-            case EAST -> new Coordinates(coordinates.getX(), coordinates.getY(), SOUTH);
-            default -> null;
-        };
+        return new Coordinates(coordinates.getX(), coordinates.getY(), RIGHT_MAP.get(coordinates.getDirection()));
     }
 
     private static Coordinates executiveLeftCommand(Coordinates coordinates) {
-        return switch (coordinates.getDirection()) {
-            case NORTH -> new Coordinates(coordinates.getX(), coordinates.getY(), WEST);
-            case SOUTH -> new Coordinates(coordinates.getX(), coordinates.getY(), EAST);
-            case WEST -> new Coordinates(coordinates.getX(), coordinates.getY(), SOUTH);
-            case EAST -> new Coordinates(coordinates.getX(), coordinates.getY(), NORTH);
-            default -> null;
-        };
+        return new Coordinates(coordinates.getX(), coordinates.getY(), LEFT_MAP.get(coordinates.getDirection()));
     }
 
     private static Coordinates executiveMoveCommand(Coordinates coordinates) {
-        return switch (coordinates.getDirection()) {
-            case NORTH -> new Coordinates(coordinates.getX(), coordinates.getY() + 1, coordinates.getDirection());
-            case SOUTH -> new Coordinates(coordinates.getX(), coordinates.getY() - 1, coordinates.getDirection());
-            case WEST -> new Coordinates(coordinates.getX() - 1, coordinates.getY(), coordinates.getDirection());
-            case EAST -> new Coordinates(coordinates.getX() + 1, coordinates.getY(), coordinates.getDirection());
-            default -> null;
-        };
+        int[] delta = MOVE_MAP.get(coordinates.getDirection());
+        return new Coordinates(coordinates.getX() + delta[0], coordinates.getY() + delta[1], coordinates.getDirection());
     }
 
 
     private static Coordinates executiveMoveBackwardCommand(Coordinates coordinates) {
-        return switch (coordinates.getDirection()) {
-            case NORTH -> new Coordinates(coordinates.getX(), coordinates.getY() - 1, coordinates.getDirection());
-            case SOUTH -> new Coordinates(coordinates.getX(), coordinates.getY() + 1, coordinates.getDirection());
-            case WEST -> new Coordinates(coordinates.getX() + 1, coordinates.getY(), coordinates.getDirection());
-            case EAST -> new Coordinates(coordinates.getX() - 1, coordinates.getY(), coordinates.getDirection());
-            default -> null;
-        };
+        int[] delta = MOVE_MAP.get(coordinates.getDirection());
+        return new Coordinates(coordinates.getX() - delta[0], coordinates.getY() - delta[1], coordinates.getDirection());
     }
 }
